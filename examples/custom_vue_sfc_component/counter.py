7b7cc96200fa6440dc2d1a31013c6b1046c26e77
David Kincaid <daelonsuzuka@gmail.com>
2024-10-30 11:22:12 +0000

    Add basic SFC example

---
+++ examples/custom_vue_sfc_component/counter.py
@@ -0,0 +1,16 @@
+from typing import Callable, Optional
+
+from nicegui.element import Element
+
+
+class Counter(Element, component='counter.vue'):
+
+    def __init__(self, title: str, *, on_change: Optional[Callable] = None) -> None:
+        super().__init__()
+        # self._props['title'] = title
+
+        self.props(f'title={title}')
+        self.on('change', on_change)
+
+    def reset(self) -> None:
+        self.run_method('reset')
---
+++ examples/custom_vue_sfc_component/counter.vue
@@ -0,0 +1,27 @@
+<template>
+    <button @click="handle_click">
+        <strong>{{ title }}: {{ value }}</strong>
+    </button>
+</template>
+<script>
+// NOTE: Make sure to reload the browser with cache disabled after making changes to this file.
+export default {
+    data() {
+        return {
+            value: 0,
+        };
+    },
+    methods: {
+        handle_click() {
+            this.value += 1;
+            this.$emit("change", this.value);
+        },
+        reset() {
+            this.value = 0;
+        },
+    },
+    props: {
+        title: String,
+    },
+};
+</script>
